d3624726470f82dd6ecff6063cf4c062dadf847e
Sergio Lima <sergiosouzalima@gmail.com>
2018-02-13 16:04:26 -0200

    reinstalled webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-// Correcao do turbolinks:load que dispara duas vezes.
-document.addEventListener("turbolinks:before-render", function() {
-  Turbolinks.clearCache()
-})
-
+console.log('Hello World from Webpacker')
